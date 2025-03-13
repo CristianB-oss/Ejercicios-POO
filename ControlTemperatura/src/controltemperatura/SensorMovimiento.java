d262e638b2d673ad48d5563be550344842e91962
CristianB-oss <cristian249645@hotmail.com>
2025-03-13 15:50:12 -0500

    Add files via upload

---
+++ ControlTemperatura/src/controltemperatura/SensorMovimiento.java
@@ -0,0 +1,15 @@
+
+package controltemperatura;
+
+import java.util.Random;
+
+
+public class SensorMovimiento {
+private Random random = new Random();
+    
+public boolean detectarMovimiento() {
+return random.nextBoolean(); 
+
+} 
+
+}
